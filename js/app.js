d4294667cc668f359287423212fd0f7c4d4288d5
Michael Ngo <michael.ngo@au.pwc.com>
2016-09-29 17:43:30 +1000

    Add emtpy BEM styles

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ require('../scss/app.scss')
 App Settings
 ***********************************/
 let options = {
-  app:                    '#app',
+  app:                    document.querySelector('#app'),
   data:                   './json/playlist.json',
   spotify: {
     id:                   '6db74688ff0349308c85371275ab285a',
@@ -39,6 +39,8 @@ fetch (options.data)
       })
     )
   })
+
+  render(state, options.app)
 })
 
 /***********************************
@@ -48,9 +50,54 @@ Controller - Events
 /***********************************
 Views - Render
 ***********************************/
-function render (state, element) {
-  element.innerHTML = ``
+function renderAlbum () {
+  return `
+    <div class="album">
+      <div class="album__video">
+
+      </div>
+
+      <div class="album__track">
+        <div class="album__cd cd">
+          <div class="cd__front"></div>
+          <div class="cd__back"></div>
+        </div>
+
+        <h4 class="album__single">
+
+        </h4>
+
+        <h2 class="album__title">
+
+        </h2>
+      </div>
+    </div>
+  `
 }
 
-let app = document.querySelector(options.app)
-render(state, app)
+function renderPlaylist () {
+  let playlist = state.playlist.map((album) => {
+    return renderAlbum()
+  }).join('')
+
+  return `
+    <div class="playlist">
+      <div class="playlist__frame-top"></div>
+      <div class="playlist__frame-right"></div>
+      <div class="playlist__frame-bottom"></div>
+      <div class="playlist__frame-left"></div>
+
+      <div class="playlist__progress-bar"></div>
+      <div class="playlist__control playlist__control--prev"></div>
+      <div class="playlist__control playlist__control--next"></div>
+
+      <div class="playlist__albums">
+        ${playlist}
+      </div>
+    </div>
+  `
+}
+
+function render (state, element) {
+  element.innerHTML = renderPlaylist()
+}
