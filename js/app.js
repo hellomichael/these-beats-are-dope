39e7f561055580361dc05591db3b024f72d1e564
Michael Ngo <michael.ngo@au.pwc.com>
2016-10-12 02:56:56 +1100

    Add multiple youtube videos

--- js/app.js
+++ js/app.js
@@ -30,8 +30,28 @@ fetch(options.json)
     let promise = fetch(`https://api.spotify.com/v1/tracks/${album.spotifyID}`)
     .then(response => response.json())
     .then(data => {
-      videos.push(new YouTube('player', {
-        videoId: album.youtubeID,
+      albums.push(new Album(data))
+    })
+
+    promises.push(promise)
+  })
+
+  Promise.all(promises).then(data => {
+    // Create playlist
+    let playlist = new Playlist({app: options.app, albums})
+
+    // Render playlist
+    options.app.innerHTML = playlist.render()
+
+    // Render youtube
+    let slides = document.querySelectorAll('.playlist__slide')
+
+    Array.from(slides).map((slide, index) => {
+      let videoNode = slide.querySelector('.video')
+      let videoID = json.albums[index].youtubeID
+
+      let video = new YouTube(videoNode, {
+        videoId: videoID,
         width: window.innerWidth,
         height: window.innerHeight + 600,
         playerVars: {
@@ -42,19 +62,10 @@ fetch(options.json)
           showInfo: 0,
           iv_load_policy: 3,
         }
-      }))
+      })
 
-      albums.push(new Album(data))
+      video.stopVideo()
+      videos.push(video)
     })
-
-    promises.push(promise)
-  })
-
-  Promise.all(promises).then(data => {
-    // Create playlist
-    let playlist = new Playlist({app: options.app, albums, videos})
-
-    // Render playlist
-    options.app.innerHTML = playlist.render()
   })
 })
