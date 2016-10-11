24412becb0f4b3e619dea507dad2ae7711ff4769
Michael Ngo <michael.ngo@au.pwc.com>
2016-10-12 02:16:32 +1100

    Add youtube video

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 Imports
 ***********************************/
 import Playlist from './Playlist.js'
+import YouTube from 'youtube-player'
 import Album from './Album.js';
 require('../scss/app.scss')
 
@@ -21,6 +22,7 @@ fetch(options.json)
 .then(response => response.json())
 .then(json => {
   let albums = []
+  let videos = []
   let promises = []
 
   // Fetch album data
@@ -28,6 +30,20 @@ fetch(options.json)
     let promise = fetch(`https://api.spotify.com/v1/tracks/${album.spotifyID}`)
     .then(response => response.json())
     .then(data => {
+      videos.push(new YouTube('player', {
+        videoId: album.youtubeID,
+        width: window.innerWidth,
+        height: window.innerHeight + 600,
+        playerVars: {
+          autoplay: 1,
+          controls: 0,
+          modestbranding: 1,
+          rel: 0,
+          showInfo: 0,
+          iv_load_policy: 3,
+        }
+      }))
+
       albums.push(new Album(data))
     })
 
@@ -36,7 +52,7 @@ fetch(options.json)
 
   Promise.all(promises).then(data => {
     // Create playlist
-    let playlist = new Playlist({app: options.app, albums})
+    let playlist = new Playlist({app: options.app, albums, videos})
 
     // Render playlist
     options.app.innerHTML = playlist.render()
