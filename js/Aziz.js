82c3851c10284bbcb5d6df1a368d9f41e32489c6
Michael Ngo <michael.ngo@au.pwc.com>
2016-10-19 02:37:09 +1100

    Add highlight styles

--- js/Aziz.js
+++ js/Aziz.js
@@ -5,17 +5,27 @@ export default class Aziz {
     Object.assign(this, options)
   }
 
+  renderHighlight(className, copy) {
+    return (`
+      <span class="aziz__highlight aziz__highlight--${className}">
+        <span class="aziz__highlight__copy">${copy}</span>
+        <span class="aziz__highlight__mask">${copy}</span>
+        <img class="aziz__highlight__stroke" src="../img/aziz-highlight.png" alt=""/>
+      </span>
+    `)
+  }
+
   render() {
     return (`
       <div class="aziz">
-        <h2 class="aziz__paragraph">I hear <span class="aziz__highlight">"808s and Heartbreak"</span> playing which is his album and I walk in and he's just sitting there in his living room.</h2>
+        <h2 class="aziz__paragraph">I hear ${this.renderHighlight('heartbreak', '"808s and Heartbreak"')}, playing which is his album and I walk in and he's just sitting there in his living room.</h2>
 
-        <h2 class="aziz__paragraph">just like bobbing his head like, <br/>
-        <span class="aziz__highlight">"boom boom boom".</span></h2>
+        <h2 class="aziz__paragraph">just like bobbing his head like, <br/>${this.renderHighlight('boom', '"boom boom boom".')}</h2>
 
-        <h2 class="aziz__paragraph">I was like,</h2>
+        <h2 class="aziz__paragraph">I was like.</h2>
 
-        <h2 class="aziz__paragraph">"Hey Kanye. Are you sitting in your own house, <span class="aziz__highlight">bobbing your own head</span>, to your own music".</h2>
+        <h2 class="aziz__paragraph">"Hey Kanye. Are you sitting in your own house,
+        ${this.renderHighlight('bobbing', 'bobbing your own head')}, to your own music".</h2>
 
         <h2 class="aziz__paragraph">And he goes...</h2>
       </div>
