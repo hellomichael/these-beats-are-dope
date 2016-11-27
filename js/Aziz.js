65133fdfb05d0b1e57e37c5c936df7d0dbc999a5
hellomichael <michael@hellomichael.com>
2016-11-27 23:33:26 +1100

    Update Kanye and Aziz to extend Animation class

--- js/Aziz.js
+++ js/Aziz.js
@@ -1,8 +1,25 @@
+import Animation from './Animation'
 require('../scss/_aziz.scss')
 
-export default class Aziz {
+export default class Aziz extends Animation {
   constructor(options) {
+    super()
     Object.assign(this, options)
+  }
+
+  componentDidMount() {
+    this.dom = {
+      azizQuote:            document.querySelector('.aziz__quote'),
+      azizHeartbreak:       document.querySelector('.aziz__highlight--heartbreak'),
+      azizBoom:             document.querySelector('.aziz__highlight--boom'),
+      azizBobbing:          document.querySelector('.aziz__highlight--bobbing'),
+      azizThese:            document.querySelector('.aziz__heading__these'),
+      azizBeats:            document.querySelector('.aziz__heading__beats'),
+      azizAre:              document.querySelector('.aziz__heading__are'),
+      azizDope:             document.querySelector('.aziz__heading__dope'),
+      azizSubheading:       document.querySelector('.aziz__subheading'),
+      azizButton:           document.querySelector('.aziz__button'),
+    }
   }
 
   showThese() {
@@ -30,48 +47,9 @@ export default class Aziz {
     this.dom.azizButton.style.display = 'inline-block'
   }
 
-  componentDidMount() {
-    // Update Dom
-    this.dom = {
-      azizQuote:            document.querySelector('.aziz__quote'),
-      azizHeartbreak:       document.querySelector('.aziz__highlight--heartbreak'),
-      azizBoom:             document.querySelector('.aziz__highlight--boom'),
-      azizBobbing:          document.querySelector('.aziz__highlight--bobbing'),
-      azizThese:            document.querySelector('.aziz__heading__these'),
-      azizBeats:            document.querySelector('.aziz__heading__beats'),
-      azizAre:              document.querySelector('.aziz__heading__are'),
-      azizDope:             document.querySelector('.aziz__heading__dope'),
-      azizSubheading:       document.querySelector('.aziz__subheading'),
-      azizButton:           document.querySelector('.aziz__button'),
-    }
-  }
-
-  renderHighlight(className, copy) {
-    return (
-      `<span class="aziz__highlight aziz__highlight--${className}">
-        <span class="aziz__highlight__copy">${copy}</span>
-        <span class="aziz__highlight__mask">${copy}</span>
-        <img class="aziz__highlight__stroke" src="../img/aziz-highlight.png" alt=""/>
-      </span>`
-    )
-  }
-
   render() {
     return (`
-      <div class="aziz">
-        <!-- <div class="aziz__quote">
-          <h2 class="aziz__paragraph">I hear ${this.renderHighlight('heartbreak', '"808s and Heartbreak"')}, playing which is his album and I walk in and he's just sitting there in his living room.</h2>
-
-          <h2 class="aziz__paragraph">just like bobbing his head like, <br/>${this.renderHighlight('boom', '"boom boom boom".')}</h2>
-
-          <h2 class="aziz__paragraph">I was like.</h2>
-
-          <h2 class="aziz__paragraph">"Hey Kanye. Are you sitting in your own house,
-          ${this.renderHighlight('bobbing', 'bobbing your own head')}, to your own music".</h2>
-
-          <h2 class="aziz__paragraph" style="margin: 0;">And he goes...</h2>
-        </div> -->
-
+      <div class="aziz aziz-${this.id}">
         <div class="aziz__intro">
           <h1 class="aziz__heading">
             <span class="aziz__heading__these">These</span>
---
+++ js/Kanye.js
@@ -0,0 +1,152 @@
+import Animation from './Animation'
+import * as Utils from './Utils.js'
+require('../scss/_kanye.scss')
+
+export default class Kanye extends Animation {
+  constructor(options) {
+    super()
+
+    this.pixiStage = null
+    this.pixiLoader = null
+    this.pixiRenderer = null
+    this.pixiScale = 1
+    this.pixiAnimation = null
+    this.requestAnimationFrame = null
+
+    this.kanyeWidth = 1884
+    this.kanyeHeight = 1937
+    this.kanyeBopCount = 0
+
+    Object.assign(this, options)
+
+    // Events
+    this.handleResize()
+  }
+
+  handleResize() {
+    window.addEventListener('resize', event => {
+      this.resizeRenderer()
+    })
+  }
+
+  resizeRenderer() {
+    this.pixiScale = Utils.getTwoDecimalPlaces((window.innerHeight + 20)/this.kanyeHeight)
+    this.pixiRenderer.view.style.transform = `scale(${this.pixiScale}) translateX(-50%)`
+  }
+
+  isReady() {
+    return new Promise((resolve, reject) => {
+      this.pixiLoader.once('complete', event => {
+        resolve(event)
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.element = document.querySelector(`.kanye-${this.id}`)
+
+    // Create renderer
+    this.pixiStage = new PIXI.Container()
+    this.pixiRenderer = PIXI.autoDetectRenderer(this.kanyeWidth + 10, this.kanyeHeight, {transparent: true})
+    this.element.appendChild(this.pixiRenderer.view)
+
+    // Resize renderer
+    this.resizeRenderer()
+
+    // Add assets
+    this.pixiLoader = PIXI.loader
+      .add(`animation-${this.id}`, 'img/kanye.json')
+      .load((loader, res) => {
+        this.kanye = new PIXI.spine.Spine(res[`animation-${this.id}`].spineData)
+        this.kanye.position.x = this.kanyeWidth/2
+        this.kanye.position.y = this.kanyeHeight
+        this.pixiStage.addChild(this.kanye)
+      })
+  }
+
+  stopAnimation() {
+    setTimeout(() => {
+      this.kanyeBopCount = 0
+      cancelAnimationFrame(this.requestAnimationFrame)
+      clearTimeout(this.pixiAnimation)
+    }, 250)
+  }
+
+  playAnimation() {
+    this.pixiAnimation = setTimeout(() => {
+      this.requestAnimationFrame = requestAnimationFrame(this.playAnimation.bind(this))
+      this.pixiRenderer.render(this.pixiStage)
+    }, 1000/60)
+  }
+
+  breathing() {
+    console.log('Breathing')
+    this.kanye.state.setAnimation(0, 'breathing', true)
+  }
+
+  bop() {
+    console.log('Bop')
+    this.kanye.state.setAnimation(0, 'bop', false)
+
+    if (this.kanyeBopCount%12 === 0) {
+      this.blink()
+    }
+
+    this.kanyeBopCount++
+  }
+
+  bopAngle() {
+    console.log('Bop Angle')
+    if (this.kanyeBopCount%12 === 0) {
+      this.blink()
+    }
+
+    if (this.kanyeBopCount%2 === 0) {
+      this.kanye.state.setAnimation(0, 'bopLeft', false)
+    }
+
+    else {
+      this.kanye.state.setAnimation(0, 'bopRight', false)
+    }
+
+    this.kanyeBopCount++
+  }
+
+  bopLeft() {
+    console.log('Bop Left')
+    this.kanye.state.setAnimation(0, 'bopLeft', false)
+  }
+
+  bopRight() {
+    console.log('Bop Right')
+    this.kanye.state.setAnimation(0, 'bopRight', false)
+  }
+
+  blink() {
+    console.log('Blink')
+    this.kanye.state.setAnimation(1, 'blink', false)
+  }
+
+  closeEyes() {
+    console.log('Close Eyes')
+    this.kanye.state.setAnimation(1, 'closeEyes', false)
+  }
+
+  openEyes() {
+    console.log('Open Eyes')
+    this.kanye.state.setAnimation(1, 'openEyes', false)
+  }
+
+  shiver() {
+    console.log('Shiver')
+    this.kanye.state.setAnimation(1, 'shiver', false)
+  }
+
+  render() {
+    return (`
+      <div class="kanye kanye-${this.id}"></div>
+    `)
+
+    return this.componentDidMount()
+  }
+}
